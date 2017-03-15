da74d0eea2c583345f036b6d73ffd26894eda270
CalabreseC <calabresec@gilmour.org>
2017-03-15 11:20:44 -0400

    added GoPiggy

--- student.py
+++ student.py
@@ -278,3 +278,8 @@ def quit():
 ##################################################################
 ######## The app starts right here when we instantiate our GoPiggy
 
+try:
+    g = GoPiggy()
+except (KeyboardInterrupt, SystemExit):
+    from gopigo import *
+    stop()
